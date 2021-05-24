2c67310f3ed0827ae1b04ff58a9a744251efad65
pineapple126 <824090342@qq.com>
2021-05-24 15:06:53 +0800

    导入自定义组件、动画样式和初始样式

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/custom-component"; // 注册自定义组件
+
+import "@/styles/animate.css";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/styles/reset.css";
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
